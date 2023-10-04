a26eaf82e6b18732d29d3c782b03c276bf862e51
congson15 <congsonver1@gmail.com>
2023-10-04 21:42:00 +0700

    chore: refactor product code

--- products/services.py
+++ products/services.py
@@ -3,28 +3,48 @@ from .serializers import ProductSerializer
 from .db import get_one, get_all
 
 
+def create_product(request):
+    serializer = ProductSerializer(data=request.data)
+
+    if not serializer.is_valid():
+        return None, serializer.errors
+
+    serializer.save()
+
+    return serializer.data, None
+
+
 def get_product(product_id):
     query = {
         "id": product_id
     }
+
     return get_one(query)
 
 
 def get_products(request):
     param_names = ['name', 'price', 'deleted']
-    valid_params, invalid_params = validate_query_params(request, param_names)
 
-    serializer = ProductSerializer(data=get_all(valid_params), many=True)
+    valid_params, _ = validate_query_params(request, param_names)
+
+    products = get_all(valid_params)
+
+    serializer = ProductSerializer(data=products, many=True)
 
     serializer.is_valid()
+
     return serializer.data
 
 
 def update_product(product, data):
+
+    product.deleted = True
+
     serializer = ProductSerializer(instance=product, data=data, partial=True)
 
     if not serializer.is_valid():
         return None, serializer.errors
 
     serializer.save()
+
     return serializer.data, None
--- products/views.py
+++ products/views.py
@@ -4,29 +4,32 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .serializers import ProductSerializer, ProductDetailDeserializer
-from .services import get_product, get_products, update_product
+from .services import get_product, get_products, create_product, update_product
 
 
 # Create your views here.
 class ProductListApiView(APIView):
 
     def get(self, request, *args, **kwargs):
-        return Response(get_products(request)
-                        , status=status.HTTP_200_OK)
+
+        products = get_products(request)
+
+        return Response(products, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
-        serializer = ProductSerializer(data=request.data)
 
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        result, errors = create_product(request)
 
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if errors:
+            return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(result, status=status.HTTP_200_OK)
 
 
 class ProductDetailApiView(APIView):
 
     def handle_get(self, product_id):
+
         product_instance = get_product(product_id)
 
         if not product_instance:
@@ -34,24 +37,29 @@ class ProductDetailApiView(APIView):
                 {"message": "Not found"},
                 status=status.HTTP_400_BAD_REQUEST
             )
+
         return product_instance
 
     def get(self, request, product_id, *args, **kwargs):
+
         product_instance = self.handle_get(product_id)
 
         if isinstance(product_instance, Response):
             return product_instance
 
         serializer = ProductDetailDeserializer(product_instance)
+
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, product_id, *args, **kwargs):
+
         product_instance = self.handle_get(product_id)
 
         if isinstance(product_instance, Response):
             return product_instance
 
         data = request.data
+
         result, errors = update_product(product_instance, data)
 
         if errors:
@@ -60,14 +68,14 @@ class ProductDetailApiView(APIView):
         return Response(result, status=status.HTTP_200_OK)
 
     def delete(self, request, product_id, *args, **kwargs):
+
         product_instance = self.handle_get(product_id)
 
         if isinstance(product_instance, Response):
             return product_instance
 
-        product_instance.deleted = True
-
         data = request.data
+
         result, errors = update_product(product_instance, data)
 
         if errors:
